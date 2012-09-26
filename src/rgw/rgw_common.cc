ec9c313d709cd872d6ffb9fc742082b88b0784c3
Yehuda Sadeh <yehuda@inktank.com>
2012-09-26 16:41:17 -0700

    rgw: get bucket logging stub
    Trivial implementation that just returns that
    bucket logging is not enabled.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -370,6 +370,7 @@ int XMLArgs::parse()
 
       if ((name.compare("acl") == 0) ||
           (name.compare("location") == 0) ||
+          (name.compare("logging") == 0) ||
           (name.compare("delete") == 0) ||
           (name.compare("uploads") == 0) ||
           (name.compare("partNumber") == 0) ||
