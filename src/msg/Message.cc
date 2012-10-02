5241e29322162a51fc558c0c4e58f6c3bdc7a200
Mike Ryan <mike.ryan@inktank.com>
2012-10-02 15:19:04 -0700

    message: add MRecoveryReserve
    This message will be used to reserve and release recovery slots on
    replica PGs.
    
    Signed-off-by: Mike Ryan <mike.ryan@inktank.com>

--- src/msg/Message.cc
+++ src/msg/Message.cc
@@ -45,6 +45,7 @@ using namespace std;
 #include "messages/MCommand.h"
 #include "messages/MCommandReply.h"
 #include "messages/MBackfillReserve.h"
+#include "messages/MRecoveryReserve.h"
 
 #include "messages/MRoute.h"
 #include "messages/MForward.h"
@@ -326,6 +327,9 @@ Message *decode_message(CephContext *cct, ceph_msg_header& header, ceph_msg_foot
     break;
   case MSG_OSD_BACKFILL_RESERVE:
     m = new MBackfillReserve;
+    break;
+  case MSG_OSD_RECOVERY_RESERVE:
+    m = new MRecoveryReserve;
     break;
 
   case MSG_ROUTE:
